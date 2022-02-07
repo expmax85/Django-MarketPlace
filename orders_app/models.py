fe70d59ea96c3402ed7bb7a0c0f3854a809bffce
Михаил Носков <rjd@yandex.ru>
2022-02-07 12:46:28 +0100

    User instead Customer in 'customer' field of Order model Calculated 'total_sum' - method in Order model Calculated 'position_price' - method in OrderProduct model

--- orders_app/models.py
+++ orders_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db.models import F, Sum
 from profiles_app.models import Customer
 from stores_app.models import SellerProduct
+from profiles_app.models import User
 
 
 class Order(models.Model):
@@ -15,6 +17,11 @@ class Order(models.Model):
     #  status  TODO определить. Например: корзина, оформленный заказ, оплаченный заказ
     created = models.DateTimeField(auto_now_add=True, null=True)
 
+    @property
+    def total_sum(self):
+        total = self.order_products.aggregate(total=Sum(F('position_price') * F('quantity')))['total']
+        return total
+
 
 class OrderProduct(models.Model):
     """
@@ -24,3 +31,9 @@ class OrderProduct(models.Model):
     seller_product = models.ForeignKey(SellerProduct, on_delete=models.CASCADE, related_name='order_products')
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(null=True)
+
+    @property
+    def position_price(self):
+        # final_price = get_discounted_price(self)   Получение цены со скидкой из сервиса скидок.
+        # Пока цена магазина. Название метода получения цкны со скидкой пока условное
+        return self.seller_product.price
