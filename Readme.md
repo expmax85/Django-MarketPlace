a5ed0286e9fb74b93f5d1ab54b2e7164b19e13be
expmax <expmax85@gmail.com>
2022-02-16 01:59:18 +0500

    Add script for autoload and migrate

---
+++ Readme.md
@@ -0,0 +1,8 @@
+#Запуск автоматических миграций и загрузок фикстур
+```
+python manage.py loadscript
+```
+
+#Пароли по тестовым пользователям
+ - admin@example.com: 123456
+ - user@test.com: qawsed12
---
+++ profiles_app/management/commands/loadscript.py
@@ -0,0 +1,39 @@
+import os
+
+from django.core import management
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        management.call_command('makemigrations')
+        management.call_command('migrate')
+        management.call_command('new_site_name')
+        folder_fixture = 'fixtures/'
+        fixtures_list = self._get_list_fixtures()
+        err_list = []
+        n_iteration = len(fixtures_list) * 2
+        self.stdout.write(f'\nFIXTURES LOAD...')
+        while fixtures_list or n_iteration:
+            err_list = []
+            for item in fixtures_list:
+                try:
+                    self.stdout.write(f'Loading {item}')
+                    management.call_command('loaddata', ''.join([folder_fixture, item]))
+                    fixtures_list.remove(item)
+                except IntegrityError:
+                    self.stdout.write(f'IntegrityError when loading {item}')
+                    if item not in err_list:
+                        err_list.append(item)
+            n_iteration -= 1
+        if err_list:
+            self.stdout.write(f'Not all fixtures was loading. Check it:')
+            print([item for item in err_list])
+        else:
+            self.stdout.write(f'\nAll commands and loading was successfully!')
+
+    def _get_list_fixtures(self):
+        path = os.path.abspath('fixtures')
+        return os.listdir(path=path)
