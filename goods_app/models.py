fc571c1d60d5b3ea2f4b0672466a03d832bf827b
expmax <expmax85@gmail.com>
2022-02-21 03:04:38 +0500

    initial commit

--- goods_app/models.py
+++ goods_app/models.py
@@ -1,3 +1,6 @@
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -33,9 +36,13 @@ class Product(models.Model):
     description = models.TextField(max_length=255, null=True, verbose_name='product_description')
     average_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, verbose_name='average_price')
     comments = models.IntegerField(null=True, verbose_name='amount_of_comments')
+    rating = models.FloatField(null=True, blank=True, default=0, verbose_name='rating')
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product-detail', kwargs={'pk': self.id})
 
     class Meta:
         verbose_name = 'product'
@@ -47,7 +54,15 @@ class ProductComment(models.Model):
     Модель комментария к товару
     """
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_comments')
-    author = models.CharField(max_length=25, null=True)
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
-    content = models.TextField(max_length=255, null=True)
-    added = models.DateTimeField(auto_now_add=True, null=True)
+    author = models.CharField(verbose_name=_('author'),max_length=25, null=True)
+    content = models.TextField(verbose_name=_('content'),max_length=255, null=True)
+    added = models.DateTimeField(verbose_name=_('added'), auto_now_add=True, null=True)
+    rating = models.IntegerField(verbose_name=_('rating'), null=True, blank=True)
+
+    def __str__(self):
+        return f'Comments for {str(self.product)}'
+
+    class Meta:
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
