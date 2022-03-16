6398ec8427a1abb4f02b83ac69f7ff6cc11d487a
expmax <expmax85@gmail.com>
2022-03-16 14:47:57 +0300

    fix user save

--- profiles_app/models.py
+++ profiles_app/models.py
@@ -71,9 +71,12 @@ class User(AbstractBaseUser, PermissionsMixin):
             old_self = User.objects.get(pk=self.pk)
             if old_self.avatar and self.avatar != old_self.avatar:
                 old_self.avatar.delete(False)
-        if self.is_member('Content-manager'):
-            self.is_staff = True
-        return super(User, self).save(*args, **kwargs)
+        try:
+            if self.is_member('Content-manager'):
+                self.is_staff = True
+        except ValueError:
+            pass
+        super(User, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = _('user')
