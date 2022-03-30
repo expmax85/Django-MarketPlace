3200c655c5f980d8e564e96241684fe05a5f8051
Михаил Носков <rjd@yandex.ru>
2022-03-30 23:36:16 +0200

    Payment service with drf added after dev merge

--- payments_app/services.py
+++ payments_app/services.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404
 from orders_app.models import Order
+from django.http.response import Http404
 
 
 def process_payment(order_id: int, account: str):
-    order = get_object_or_404(Order, id=order_id)
     try:
+        order = get_object_or_404(Order, id=order_id)
+
         if int(account) % 2 == 0 and account[-1] != '0' and len(account) == 8 and not order.paid:
             order.paid = True
             order.braintree_id = account
@@ -12,17 +14,23 @@ def process_payment(order_id: int, account: str):
             return True
         return False
 
-    except TypeError:
+    except (IndexError, KeyError, ValueError, Http404):
         return False
 
 
 def check_status(order_id: int):
-    order = get_object_or_404(Order, id=order_id)
-    if order:
-        if order.in_order is True:
-            if order.paid is True:
-                return 'Paid'
-            else:
-                return 'Not Paid'
+    try:
+        order = get_object_or_404(Order, id=order_id)
+        if order:
+            if order.in_order is True:
+                if order.paid is True:
+                    return 'Paid'
+                else:
+                    return 'Not Paid'
         return f'No order No {order_id}'
-    return f'No order No {order_id}'
+
+    except (IndexError, KeyError, Http404):
+        return f'No order No {order_id}'
+
+    except ValueError:
+        return 'Order number is incorrect'
