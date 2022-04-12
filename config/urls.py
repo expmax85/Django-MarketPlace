30fb49d4b6d4c77ce8e637c066a7cdd74230bbd0
Михаил Носков <rjd@yandex.ru>
2022-04-12 12:47:22 +0200

    Swagger docs Readme sample for discounts app

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,23 @@ from django.urls import path, include
 
 from settings_app.views import AdminView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MEGANO payment API",
+      default_version='v1',
+      description="This api is used for getting payments through random account",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="megano@megano.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('i18n', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
@@ -35,4 +52,5 @@ urlpatterns = [
     path('stores/', include('stores_app.urls', namespace='stores-polls')),
     path('discounts/', include('discounts_app.urls', namespace='discounts-polls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
